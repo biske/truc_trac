740d8282255e895e582a8fb12d9dede93d7de792
Иван Бишевац <ivan.bisevac@gmail.com>
2013-11-03 01:35:10 +0100

    Add some documentation

--- lib/truc_trac.rb
+++ lib/truc_trac.rb
@@ -1,4 +1,11 @@
+# Main class
 class TrucTrac
+    # Say Трућ траћ to the world
+    #
+    # Example:
+    #   >> TrucTrac.truc_trac
+    #   => "Трућ траћ"
+    #
     def self.truc_trac
         "Трућ траћ"
     end
